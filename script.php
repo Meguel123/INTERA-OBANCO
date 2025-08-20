14b73fcca06e2db9bbdb3d5b958ebf62ce1c3141
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-08-20 15:11:16 -0300

    s

--- script.php
+++ script.php
@@ -7,37 +7,44 @@ $database = "db_escola";
 $connecta = new mysqli($server, $user, $password);
 $açao = $_POST['acao'];
 
+if ($connecta == true) {
+    echo"<script>alert('CONECTADO'); history.back()</script>";
+}
+
 if ($açao == "criar")
 { 
-    $sql = "create database $database";
-
-
-    if($connecta->query($sql) == true){
-
-        echo"<script>alert('banco criado'); history.back()</script>";
-
-    }
+    $sql = "CREATE TABLE tb_escola(id_aluno int auto_increment primary key,
+    nm_aluno varchar(45),
+    idade int)";
+    sleep(3);
+  $connecta->query($sql);
+    $insert = "INSERT INTO  tb_escola(NULL,'Miguel Perassoli',16)";
+    sleep(3);
+    $connecta->query($insert);
+    echo "$insert";
 }
 
 if ($açao == "buscar")
-{ 
+{  $buscar = "SELECT * FROM tb_escola"; 
+    sleep(3);
     echo"<script>alert('buscando'); history.back()</script>";
+    echo "$buscar";
 }
 
 if ($açao == "atualizando")
-{ 
+{ $atualizar = "UPDATE FROM tb_escola SET nm_aluno='Joelma' WHERE id ='1'";
+    sleep(3);
     echo"<script>alert('atualizado'); history.back()</script>";
+    echo "$atualizar";
 }
 
 if ($açao == "deletar")
-{ 
-    $sql = "delete database $database";
-    if($connecta->query($sql) == true){
-
-        echo"<script>alert('deletando banco'); history.back()</script>";
-
-    }
-  
+{
+$deletar ="DELETE FROM tb_escola";
+sleep(3);
+echo"<script>alert('DELETADO'); history.back()</script>";
+echo "$deletar";
+} 
  
-}
+
 ?>
